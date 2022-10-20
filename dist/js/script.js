d4b7d541b5cec12290ef9c8e6901c5222386bcd3
Polina Ganina <poliganina@gmail.com>
2022-10-20 10:36:57 +0300

    adaptive done

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,7 +1,7 @@
 "use strict";
 
-const menu = document.querySelector(".header__menu"),
-  logo = document.querySelector(".header__logo"),
+const header = document.querySelector(".header"),
+  menu = document.querySelector(".header__menu"),
   hamburger = document.querySelector(".hamburger");
 
 $(document).ready(function () {
@@ -15,26 +15,18 @@ $(document).ready(function () {
   });
 });
 
-// $(window).scroll(function () {
-//   if ($(this).scrollTop() > 900) {
-//     $(".scrollup").fadeIn();
+// window.addEventListener("scroll", () => {
+//   if (window.scrollY > 180) {
+//     menu.classList.add("header__menu-short");
+//     logo.classList.add("header__logo-short");
 //   } else {
-//     $(".scrollup").fadeOut();
+//     menu.classList.remove("header__menu-short");
+//     logo.classList.remove("header__logo-short");
 //   }
 // });
 
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 180) {
-    menu.classList.add("header__menu-short");
-    logo.classList.add("header__logo-short");
-  } else {
-    menu.classList.remove("header__menu-short");
-    logo.classList.remove("header__logo-short");
-  }
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("is-active");
+  header.classList.toggle("header_mobile");
+  menu.classList.toggle("header__menu_mobile");
 });
-
-// hamburger.addEventListener("click", () => {
-//   hamburger.classList.toggle("is-active");
-//   navList.classList.toggle("show");
-//   menu.classList.toggle("header__menu-hamburger");
-// });
