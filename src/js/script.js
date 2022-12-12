786dd16ba71eaae400873e3bb3866538365676f1
Polina Ganina <poliganina@gmail.com>
2022-12-12 13:35:25 +0300

    carousel fixed

--- src/js/script.js
+++ src/js/script.js
@@ -1,46 +1,42 @@
 "use strict";
 
-const header = document.querySelector(".header"),
-  menu = document.querySelector(".header__menu"),
-  hamburger = document.querySelector(".hamburger"),
-  carouselInner = document.querySelector(".carousel__inner"),
-  carouselItems = document.querySelectorAll(".carousel__item"),
-  screenWidth = window.screen.width;
+window.addEventListener("DOMContentLoaded", (event) => {
+  const hamburger = document.querySelector(".hamburger"),
+    carouselInner = document.querySelector(".carousel__inner"),
+    carouselItems = document.querySelectorAll(".carousel__item"),
+    screenWidth = window.screen.width;
 
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("is-active");
-  // header.classList.toggle("header_mobile");
-  // menu.classList.toggle("header__menu_mobile");
-});
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("is-active");
+    //add mobile menue toggling
+  });
 
-carouselInner.style.width = "fit-content";
-let carouselWidth = carouselInner.getBoundingClientRect().width;
+  carouselInner.style.width = "fit-content";
+  let carouselWidth = carouselInner.getBoundingClientRect().width;
 
-// contentWidth - is there enough content for carousel animation?
-let contentWidth = 0;
-for (let i = 0; i < carouselItems.length; i++) {
-  const width = Math.round(carouselItems[i].getBoundingClientRect().width);
-  contentWidth += width;
-}
-let vw = Math.round((contentWidth / screenWidth) * 100);
-//if vw is > 100, the contend width is enough;
-
-if (vw >= 100) {
-  //copy content for internal movement effect
-  const copy = document.createElement("div");
-  copy.style.display = "flex";
-  carouselInner.append(copy);
+  // contentWidth - is there enough content for carousel animation?
+  let contentWidth = 0;
   for (let i = 0; i < carouselItems.length; i++) {
-    let copyItem = carouselItems[i].cloneNode(true);
-    copy.append(copyItem);
+    const width = Math.round(carouselItems[i].getBoundingClientRect().width);
+    contentWidth += width;
   }
+  let vw = Math.round((contentWidth / screenWidth) * 100);
+  //if vw is > 100, the contend width is enough;
+  if (vw >= 100) {
+    //copy content for internal movement effect
+    const copy = document.createElement("div");
+    copy.style.display = "flex";
+    carouselInner.append(copy);
+    for (let i = 0; i < carouselItems.length; i++) {
+      let copyItem = carouselItems[i].cloneNode(true);
+      copy.append(copyItem);
+    }
 
-  setTimeout(() => {
     gsap.to(".carousel__inner", {
       x: `-${carouselWidth}px`,
-      duration: 40,
+      duration: 45,
       ease: "none",
       repeat: -1,
     });
-  }, 2000);
-}
+  }
+});
